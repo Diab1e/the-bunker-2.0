0fd985491aad96b8bc412202883790bffedd04a9
linoid <71238085+linoid-gaming@users.noreply.github.com>
2022-03-01 15:25:08 -0800

    my fault og

---
+++ tokyo-night/cache/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#85c4e6", "#0e0f1e" },
+	[SchemeSel] = { "#85c4e6", "#B02354" },
+	[SchemeOut] = { "#85c4e6", "#AD5E98" },
+};
