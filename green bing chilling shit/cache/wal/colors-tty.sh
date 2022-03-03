75c9ec3d1b1f36daf9ae5ca8a812021d53f63539
linoid <71238085+linoid-gaming@users.noreply.github.com>
2022-03-03 08:21:30 -0800

    green bing chilling

---
+++ green bing chilling shit/cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P001050C
+                 \e]P1B2DA6F
+                 \e]P2BAE278
+                 \e]P3444A86
+                 \e]P4B9DC85
+                 \e]P5BCE485
+                 \e]P6C6EA8E
+                 \e]P7dee9cd
+                 \e]P89ba38f
+                 \e]P9B2DA6F
+                 \e]PABAE278
+                 \e]PB444A86
+                 \e]PCB9DC85
+                 \e]PDBCE485
+                 \e]PEC6EA8E
+                 \e]PFdee9cd
+                 \ec'
---
+++ green bing chilling shit/cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#01050C";
+static const char* selfgcolor   = "#dee9cd";
+static const char* normbgcolor  = "#BAE278";
+static const char* normfgcolor  = "#dee9cd";
+static const char* urgbgcolor   = "#B2DA6F";
+static const char* urgfgcolor   = "#dee9cd";
