bdcc5d522863e274c3a5f5291f4c7e396bf587c7
Hu Tao <71238085+linoid-gaming@users.noreply.github.com>
2022-03-18 18:40:27 -0700

    fuck u

--- windows xp wallpaper/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cbdbf0", "#181c14" },
-	[SchemeSel] = { "#cbdbf0", "#8ABB0C" },
-	[SchemeOut] = { "#cbdbf0", "#90B5F2" },
-};
--- windows xp wallpaper/cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#181c14";
-static const char* selfgcolor   = "#cbdbf0";
-static const char* normbgcolor  = "#3B54A9";
-static const char* normfgcolor  = "#cbdbf0";
-static const char* urgbgcolor   = "#8ABB0C";
-static const char* urgfgcolor   = "#cbdbf0";
