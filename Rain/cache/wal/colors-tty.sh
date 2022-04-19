1f39c93d7f9031788291eb1f457742840da1be9a
linoid <71238085+linoid-gaming@users.noreply.github.com>
2022-04-18 21:32:50 -0700

    rain

---
+++ Rain/cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P00e090b
+                 \e]P16E2D2B
+                 \e]P24A3E43
+                 \e]P362555C
+                 \e]P4962925
+                 \e]P5EF322A
+                 \e]P6B35850
+                 \e]P7b9b8bb
+                 \e]P8818082
+                 \e]P96E2D2B
+                 \e]PA4A3E43
+                 \e]PB62555C
+                 \e]PC962925
+                 \e]PDEF322A
+                 \e]PEB35850
+                 \e]PFb9b8bb
+                 \ec'
---
+++ Rain/cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#0e090b";
+static const char* selfgcolor   = "#b9b8bb";
+static const char* normbgcolor  = "#4A3E43";
+static const char* normfgcolor  = "#b9b8bb";
+static const char* urgbgcolor   = "#6E2D2B";
+static const char* urgfgcolor   = "#b9b8bb";
