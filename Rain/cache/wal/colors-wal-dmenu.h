79f456ebd07ffc5f15b2cf686d1e8e9376878711
linoid <71238085+alice1212123@users.noreply.github.com>
2022-06-10 19:58:45 -0700

    kanye

--- Rain/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#b9b8bb", "#0e090b" },
-	[SchemeSel] = { "#b9b8bb", "#6E2D2B" },
-	[SchemeOut] = { "#b9b8bb", "#B35850" },
-};
