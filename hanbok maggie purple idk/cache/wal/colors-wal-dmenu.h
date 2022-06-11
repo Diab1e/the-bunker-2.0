c494264d267555f44f151cb1e6214313c322b790
linoid <71238085+alice1212123@users.noreply.github.com>
2022-06-10 19:58:58 -0700

    kanye

--- hanbok maggie purple idk/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e8e8e4", "#151315" },
-	[SchemeSel] = { "#e8e8e4", "#3B5DA4" },
-	[SchemeOut] = { "#e8e8e4", "#A8C0C6" },
-};
--- hanbok maggie purple idk/cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#151315";
-static const char* selfgcolor   = "#e8e8e4";
-static const char* normbgcolor  = "#943A9C";
-static const char* normfgcolor  = "#e8e8e4";
-static const char* urgbgcolor   = "#3B5DA4";
-static const char* urgfgcolor   = "#e8e8e4";
