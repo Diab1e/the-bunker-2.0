6f2c3deb9bea8535f96325925c316f9ef3c02433
Yoimiya <71238085+Yoimiya10101@users.noreply.github.com>
2022-10-07 23:29:30 -0700

    cleanup

--- mountian wallpaper/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c4cddb", "#060911" },
-	[SchemeSel] = { "#c4cddb", "#A17460" },
-	[SchemeOut] = { "#c4cddb", "#8C99AE" },
-};
--- sakura light/cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0a1116";
-static const char* selfgcolor   = "#e3dcd2";
-static const char* normbgcolor  = "#7B8479";
-static const char* normfgcolor  = "#e3dcd2";
-static const char* urgbgcolor   = "#906F65";
-static const char* urgfgcolor   = "#e3dcd2";
