15e46182fe5ae2c8a182a27ff3e28b27336e0a42
Yoimiya <71238085+Yoimiya10101@users.noreply.github.com>
2022-10-07 23:31:07 -0700

    ASS

--- tokyo-night/cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0e0f1e";
-static const char* selfgcolor   = "#85c4e6";
-static const char* normbgcolor  = "#063589";
-static const char* normfgcolor  = "#85c4e6";
-static const char* urgbgcolor   = "#B02354";
-static const char* urgfgcolor   = "#85c4e6";
